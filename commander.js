534fbab41235c597a98f8c01598a0103cb0b380b
Maleckim <maleckimatthewm@gmail.com>
2020-05-09 22:52:36 -0500

    able to view collections from api

--- commander.js
+++ commander.js
@@ -1,28 +1,58 @@
 const { Command } = require('commander');
 const exec = require('./lib/exec_sync');
+const fetch = require('node-fetch');
+const defer = require('lodash.defer');
 //creating and checking for persisting data
 const name = require('./lib/local_persist');
 
-const program = new Command();
+const npmm = new Command();
+
+let collections = [];
+
+function displayCollections(userCollections){
+  for(let i in userCollections){
+    console.log(`${userCollections[i].collection_name}`)
+  }
+}
+
 
 function listView() {
-    console.log(exec('ls'));
+  fetch(`http://localhost:8000/api/collections?userId=1`)
+    .then( res => {
+      if(res.ok){
+        return res.json()
+      }
+    })
+    .then( resJSON => {
+      return displayCollections(resJSON); 
+    })
+
+    // console.log(exec('ls'));
 }
 
 function viewUser() {
   console.log(name);
 }
 
-program
-  .option("-v, --view", "View all the user collections", listView)
-  .option("-u, --user", "Check current user", viewUser)
 
-program
+npmm
+  // .option("-v, --view", "View all the user collections", listView)
+  // .option("-U, --user", "Sign in for personalized view", signIn)
+  .option("-u, --who", "Check current user", viewUser)
+
+npmm
   .command("launch")
   .description("create a package-lock.json")
   .action(() => {
     console.log(exec('ls'));
   });
+
+npmm
+  .command("view")
+  .description("view user collections")
+  .action(() => {
+    listView()
+  })
   
 // 113-0668256-0077045
-program.parse(process.argv);
+npmm.parse(process.argv);
