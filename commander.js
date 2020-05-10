da624315a5395bffac89cca321183dfc8af3dddb
Maleckim <maleckimatthewm@gmail.com>
2020-05-10 03:13:16 -0500

    minor fixes

--- commander.js
+++ commander.js
@@ -1,72 +1,58 @@
-const { Command } = require('commander');
-const exec = require('./lib/exec_sync');
-const fetch = require('node-fetch');
-const defer = require('lodash.defer');
-//creating and checking for persisting data
-const {setName, retrieveName} = require('./lib/local_persist')
+const { Command } = require("commander");
+const exec = require("./lib/exec_sync");
+const fetch = require("node-fetch");
+const { setName, retrieveName } = require("./lib/local_persist");
 
 const npmm = new Command();
 
-let collections = [];
-
-function displayCollections(userCollections){
-  for(let i in userCollections){
-    console.log(`${userCollections[i].collection_name}`)
+function displayCollections(userCollections) {
+  for (let i in userCollections) {
+    console.log(`${userCollections[i].collection_name}`);
   }
 }
 
-
 function listView() {
   fetch(`http://localhost:8000/api/collections?userId=1`)
-    .then( res => {
-      if(res.ok){
-        return res.json()
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
       }
     })
-    .then( resJSON => {
-      return displayCollections(resJSON); 
-    })
+    .then((resJSON) => {
+      return displayCollections(resJSON);
+    });
 
-    // console.log(exec('ls'));
+  // console.log(exec('ls'));
 }
 
-function viewUser() {
-  getName();
-}
-
-
-npmm
-  // .option("-v, --view", "View all the user collections", listView)
-  // .option("-U, --user", "Sign in for personalized view", signIn)
-  .option("-u, --who", "Check current user", viewUser)
 
 npmm
   .command("launch")
   .description("create a package-lock.json")
   .action(() => {
-    console.log(exec('ls'));
+    console.log(exec("ls"));
   });
 
 npmm
   .command("view")
   .description("view user collections")
   .action(() => {
-    listView()
-  })
+    listView();
+  });
 
 npmm
   .command("who")
   .description("view who is signed on")
   .action(() => {
     retrieveName();
-  })
+  });
 
 npmm
   .command("set-user <email>")
   .description("set the user email for NPMM")
-  .action( (email) => {
+  .action((email) => {
     setName(email);
-  })
-  
+  });
+
 // 113-0668256-0077045
 npmm.parse(process.argv);
