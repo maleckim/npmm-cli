e3456927f16f944acb45a1c07731f93b82dccae4
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-17 13:41:56 -0700

    got collections to work

--- pman.js
+++ pman.js
@@ -1,7 +1,8 @@
 const { Command } = require("commander");
 const exec = require("./lib/execSync");
 const fetch = require("node-fetch");
-const { setName, retrieveName, getCollections, readyLaunch } = require("./lib/localPersist");
+// const { setName, retrieveName, getCollections, readyLaunch } = require("./lib/localPersist");
+const npmmAPI = require('./services/npmmAPI')
 
 const npmm = new Command();
 
@@ -11,34 +12,34 @@ function displayCollections(userCollections) {
   }
 }
 
-npmm
-  .command("launch <collection_name>")
-  .description("installs your npm packages")
-  .action((collection_name) => {
-    readyLaunch(collection_name);
-    // console.log(exec("ls"));
-  });
+// npmm
+//   .command("launch <collection_name>")
+//   .description("installs your npm packages")
+//   .action((collection_name) => {
+//     readyLaunch(collection_name);
+//     // console.log(exec("ls"));
+//   });
 
 npmm
   .command("view")
   .description("view user collections")
   .action(() => {
-     getCollections();
+    npmmAPI.getCollections();
   });
 
-npmm
-  .command("who")
-  .description("view who is signed on")
-  .action(() => {
-    retrieveName();
-  });
+// npmm
+//   .command("who")
+//   .description("view who is signed on")
+//   .action(() => {
+//     retrieveName();
+//   });
 
-npmm
-  .command("set-user <email> <password>")
-  .description("set the user email for NPMM")
-  .action((email, password) => {
-    setName(email,password);
-  });
+// npmm
+//   .command("set-user <email> <password>")
+//   .description("set the user email for NPMM")
+//   .action((email, password) => {
+//     setName(email,password);
+//   });
 
 // 113-0668256-0077045
 npmm.parse(process.argv);
--- services/npmmAPI.js
+++ services/npmmAPI.js
@@ -1,7 +1,8 @@
 const fetch = require("node-fetch");
 const {API_ENDPOINT} = require('../config')
+const store = require('../lib/localPersist')
 
-const apiObject = {
+const npmmAPI = {
   packageFromCollection(id, auth, callback){
     fetch(`${API_ENDPOINT}/api/collections/${id}?justNames=true`, 
     {
@@ -12,7 +13,23 @@ const apiObject = {
     })
     .then(res => res.ok ? res.json() : console.log('trouble'))
     .then(resJSON => callback(resJSON))
+  },
+  getCollections: async () => {
+    const token = await store.getToken();
+    fetch(`${API_ENDPOINT}/api/collections`, {
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((resJSON) => {
+          resJSON.map((collections) => {
+            console.log(collections.collection_name.replace(' ', '-'));
+          });
+          return resJSON;
+        });
   }
 }
 
-module.exports = apiObject;
+module.exports = npmmAPI;
